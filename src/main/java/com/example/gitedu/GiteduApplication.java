979b8a9c69e46e867584015758e48762699907d3
严墨楷 <769667444@qq.com>
2022-03-13 22:15:49 +0800

    comment1

---
+++ src/main/java/com/example/gitedu/GiteduApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiteduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitedu/GiteduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiteduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
